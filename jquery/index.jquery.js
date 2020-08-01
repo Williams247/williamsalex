f76a47d90b1dbbc13400c980e1e86b888fafe140
Williams Alex <44811175+Williams247@users.noreply.github.com>
2020-08-01 17:12:43 +0100

    Update index.jquery.js

--- jquery/index.jquery.js
+++ jquery/index.jquery.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     setTimeout(() => {
         $('body').css({overflow:'visible'});
+        $('#skills-modal').addClass('hidden');
         $('.modal-body').fadeOut();
     }, 4800);
 });
